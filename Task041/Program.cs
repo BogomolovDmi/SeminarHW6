4f7d6db8c5c3db1095546b4d62442a6d63ce5c8b
DmitryB <BogomolovDME@yandex.ru>
2022-08-26 11:42:47 +0300

    Complite

---
+++ Task041/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите размер массива");
+int m = int.Parse(Console.ReadLine()!);
+
+int[] createArray()
+{
+    int [] result = new int[m];
+    
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.WriteLine($"Введите символ с индексом {i}");
+        result[i] = int.Parse(Console.ReadLine()!);
+    }
+
+    return result;
+}
+
+int searchNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result += 1;
+        }
+    }
+    return result;
+}
+
+void printArray1(int[] array) 
+{ 
+    Console.Write("["); 
+ 
+    for (var i = 0; i < array.Length; i++) 
+    { 
+        Console.Write(array[i]); 
+ 
+        if (i != array.Length - 1) 
+            Console.Write(", "); 
+    } 
+ 
+    Console.Write("]"); 
+    Console.WriteLine(); 
+} 
+Console.WriteLine("Вводите массив по 1 символу");
+Console.WriteLine();
+var array = createArray(); 
+printArray1(array);
+int result = searchNumbers(array);
+Console.Write(" -> " + result);
+
+
+
+
---
+++ Task043/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+float x = 0;
+float y = 0;
+
+Console.WriteLine("Введите b1");
+float b1 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите k1");
+float k1 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите b2");
+float b2 = float.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите k2");
+float k2 = float.Parse(Console.ReadLine()!);
+
+if ((k1 == k2) && (b1 == b2))
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    Console.WriteLine("Точка пересечения прямых будет иметь координату (" + x + "; " + y + ")");
+}
+
+
